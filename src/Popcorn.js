b8635cffd946309cf9f7b66ffb099b13dcd26b6b
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-08-31 16:48:55 -0700

    implemented nav bar and a little styling

--- src/Popcorn.js
+++ src/Popcorn.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Popcorn.css';
 
 /**Shows Popcorn
  * 
@@ -7,16 +8,59 @@ import { Link } from 'react-router-dom';
  * -None
  * 
  * State:
- * -None
+ * -popcornCoords [{xValue, yValue},...]
  * 
  * App -> Route(/popcorn) -> Popcorn
  */
 function Popcorn(){
+  const popcornImgURL = 'https://www.vhv.rs/dpng/d/3-39613_single-popcorn-png-transparent-png.png';
+  const [popcornCoords, setPopcornCoords] = useState([]);
+  
+  function handleChange(evt) {
+    const xValue = Math.floor(Math.random()*100);
+    const yValue = Math.floor(Math.random()*100);
+    setPopcornCoords( popcornCoords => [...popcornCoords, {xValue, yValue}])
+  }
+
+  function renderPopcorn() {
+    return popcornCoords.map( popcornCoords =>(
+      <img
+        src={popcornImgURL}
+        alt='Popcorn'
+        style={{
+          top:`${popcornCoords.xValue}vh`, 
+          left: `${popcornCoords.yValue}vw`,
+          position: 'absolute',
+          width: '200px',
+          height: 'auto',
+          backgroundColor: 'transparent'
+        }}
+        />
+    ))
+    // const imgArray = [];
+    // for(let i=0; i<popcornCount; i++) {
+    //   const xValue = Math.floor(Math.random()*100);
+    //   const yValue = Math.floor(Math.random()*100);
+    //   const imgStyle={
+    //     position: 'absolute',
+    //     width: '200px',
+    //     height: 'auto',
+    //     left: `${xValue}vw`,
+    //     top: `${yValue}vh`
+    //   }
+    //   imgArray.push(<img src={popcornImgURL} style={imgStyle} alt='popcorn'/>)
+    // }
+    // console.log('imgArray', imgArray);
+    // return imgArray
+  }
 
   return(
-    <div>
+    <div className='Popcorn'>
       <h1>Pop...Pop...Pop</h1>
-      <Link to='/'>Go Back</Link>
+      <Link to='/' className='btn btn-warning'>Go Back</Link>
+      {renderPopcorn()}
+      <button onClick={handleChange}>How many pieces?</button>
+  <p>Pieces: {popcornCoords.length}</p>
     </div>
   )
 }
